f41544db43a79e6e5d5efa9eb2e2e9b57d21b508
Derry Cheng <derryc09@uw.edu>
2020-09-22 22:48:58 -0700

    adding environemnt keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCY-HiJDGT6feS_V1BzWZkmo6bIItW0Syk",
+    authDomain: "companies-aka.firebaseapp.com",
+    databaseURL: "https://companies-aka.firebaseio.com",
+    projectId: "companies-aka",
+    storageBucket: "companies-aka.appspot.com",
+    messagingSenderId: "414073819941",
+    appId: "1:414073819941:web:fd0c59f4bcfb11787180cc",
+    measurementId: "G-NG6K073TMR"
+  }
 };
